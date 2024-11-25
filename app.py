ddb7d6c745b66b5f98eb0651bc87bf65fe26b42a
techyberme <107142731+techyberme@users.noreply.github.com>
2024-11-25 14:33:36 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from config import config
 app = Flask(__name__)
 conexion= MySQL(app)
 
-##listar
+##lista
 @app.route('/eventos', methods= ['GET'])
 def listar_eventos():
     try:
@@ -22,7 +22,7 @@ def listar_eventos():
 
     except Exception as ex:
         return jsonify({'mensaje': ex})
-
+    
 @app.route('/usuarios', methods= ['GET'])
 def listar_usuarios():
     try:
@@ -30,16 +30,17 @@ def listar_usuarios():
         sql = "SELECT * FROM usuarios"
         cursor.execute(sql)
         datos= cursor.fetchall()
-        usuario =[]
+        usuarios =[]
         for fila in datos:
             usuario={'ID_USUARIO': fila[0],'NOMBRE_USUARIO': fila[1],'CORREO':str(fila[2]),
                       'CONTRASEÑA':fila[3],'FECHA_NAC': str(fila[4]),'SEXO':fila[5]}
-            usuario.append(usuario)
+            print(usuario)
+            usuarios.append(usuario)
         print(datos)
-        return jsonify({'usuario': usuario ,'mensaje': 'usuarios listados'})
+        return jsonify({'usuario': usuarios ,'mensaje': 'usuarios listados'})
 
     except Exception as ex:
-        return jsonify({'mensaje': ex})
+        return jsonify({'mensaje': ex})
                         
 @app.route('/deportes', methods= ['GET'])
 def listar_deportes():
@@ -48,15 +49,15 @@ def listar_deportes():
         sql = "SELECT * FROM deportes"
         cursor.execute(sql)
         datos= cursor.fetchall()
-        deporte =[]
+        deportes =[]
         for fila in datos:
             deporte={'NOMBRE_DEPORTE': fila[0],'TIPO_DEPORTE': fila[1],'EQUIPO':fila[2]}
-            deporte.append(deporte)
+            deportes.append(deporte)
         print(datos)
-        return jsonify({'deporte': deporte ,'mensaje': 'deportes listados'})
+        return jsonify({'deporte': deportes ,'mensaje': 'deportes listados'})
 
     except Exception as ex:
-        return jsonify({'mensaje': ex})
+        return jsonify({'mensaje': ex})
                         
 
 @app.route('/usuario_actividad', methods= ['GET'])
@@ -66,128 +67,83 @@ def listar_usuario_actividad():
         sql = "SELECT * FROM USUARIOACTIVIDAD"
         cursor.execute(sql)
         datos= cursor.fetchall()
-        usuario_actividad =[]
+        usuario_actividades =[]
         for fila in datos:
             usuario_actividad={'ID_USUARIO': fila[0],'ID_ACTIVIDAD': fila[1]}
-            usuario_actividad.append(usuario_actividad)
+            usuario_actividades.append(usuario_actividad)
         print(datos)
-        return jsonify({'usuario_actividad': usuario_actividad ,'mensaje': 'usuario_actividad'})
+        return jsonify({'usuario_actividad': usuario_actividades ,'mensaje': 'usuario_actividad'})
 
     except Exception as ex:
-        return jsonify({'mensaje': ex})
-                        
-
-#eventos
-@app.route('/cursos', methods= ['POST'])
-def registrar_evento():
-    #print(request)
+        return jsonify({'mensaje': ex})
+    
+@app.route('/actividades', methods= ['GET'])
+def listar_actividades():
     try:
-        cursor = conexion.connection.cursor()
-        sql = """INSERT INTO eventos
-(ID_EVENTO, FECHA, HORA, PRECIO, LOCALIZACION,PLAZAS)
-VALUES ('{0}','{1}','{2}','{3}','{4}','{5}') """.format(request.json ['evento'],
-request.json['fecha'],
+        cursor =conexion.connection.cursor()
+        sql = "SELECT * FROM ACTIVIDADES"
+        cursor.execute(sql)
+        datos= cursor.fetchall()
+        actividades =[]
+        for fila in datos:
+            actividad={'ID_ACTIVIDAD': fila[0],'NOMBRE_ACTIVIDAD': fila[1],'TEXTO': fila[2],
+                       'KM': fila[3],'FC': fila[4],'KCAL': fila[5],'DURACION': str(fila[6]),
+                       'ID_EVENTO': fila[7],'NOMBRE_DEPORTE': fila[8]}
+            actividades.append(actividad)
+        print(datos)
+        return jsonify({'actividad': actividades ,'mensaje': 'actividad'})
 
-request.json['hora'],
-
-request.json['precio'],
-
-request.json['localizacion'],
-
-request.json['plazas']
-            
-            )
-        
-        cursor. execute(sql)
-        conexion.connection.commit()
-#confirma acción inserción
-    #print(request.json)
-        return jsonify({'mensaje': 'evento registrado'})
     except Exception as ex:
-        return jsonify({'mensaje': "error"})
+        return jsonify({'mensaje': ex})
+
+
+                        
+##REGISTROS
+
+#eventos 
+
+@app.route('/eventos', methods= ['POST']) 
+def registrar_evento(): 
+#print(request) 
+    try: 
+        cursor = conexion.connection.cursor() 
+        sql = """INSERT INTO eventos 
+        (ID_EVENTO, FECHA, HORA, PRECIO, LOCALIZACION, PLAZAS) 
+        VALUES ('{0}','{1}','{2}','{3}','{4}','{5}') """.format(request.json ['evento'], 
+        request.json['fecha'], 
+        request.json['hora'], 
+        request.json['precio'], 
+        request.json['localizacion'], 
+        request.json['plazas'] 
+        ) 
+        cursor. execute(sql) 
+        conexion.connection.commit() 
+
+        #confirma acción inserción
+        #print(request.json) 
+
+        return jsonify({'mensaje': 'evento registrado'}) 
+
+    except Exception as ex: 
+        return jsonify({'mensaje': "error"}) 
     
 
-#usuarios
-@app.route('/cursos', methods= ['POST'])
-def registrar_usuario():
-    #print(request)
-    try:
-        cursor = conexion.connection.cursor()
-        sql = """INSERT INTO usuarios
-(ID_USUARIO, NOMBRE_USUARIO, CORREO, CONTRASEÑA, FECHA_NAC,SEXO)
-VALUES ('{0}','{1}','{2}','{3}','{4}','{5}') """.format(request.json ['id del usuario'],
-request.json['nombre de usuario'],
-
-request.json['direccion de correo'],
-
-request.json['contraseña'],
-
-request.json['fecha nacimiento'],
-
-request.json['sexo']
-            
-            )
-        
-        cursor. execute(sql)
-        conexion.connection.commit()
-#confirma acción inserción
-    #print(request.json)
-        return jsonify({'mensaje': 'usuario registrado'})
-    except Exception as ex:
-        return jsonify({'mensaje': "error"})
-
-
-#deportes
-@app.route('/cursos', methods= ['POST'])
-def registrar_deporte():
-    #print(request)
-    try:
-        cursor = conexion.connection.cursor()
-        sql = """INSERT INTO deportes
-(NOMBRE_DEPORTE, TIPO_DEPORTE, EQUIPO)
-VALUES ('{0}','{1}','{2}') """.format(request.json ['nombre del deporte'],
-request.json['tipo de deporte'],
-
-request.json['equipo']
-            
-            )
-        
-        cursor. execute(sql)
-        conexion.connection.commit()
-#confirma acción inserción
-    #print(request.json)
-        return jsonify({'mensaje': 'deporte registrado'})
-    except Exception as ex:
-        return jsonify({'mensaje': "error"})
-
-
-
-
-#actividades
-@app.route('/cursos', methods= ['POST'])
+@app.route('/actividades', methods= ['POST'])
 def registrar_actividad():
     #print(request)
     try:
         cursor = conexion.connection.cursor()
         sql = """INSERT INTO actividades
-(ID_ACTIVIDAD, NOMBRE_ACTIVIDAD, TEXTO, KM, FRECUENCIACARDIACA,KCAL, DURACION, ID_EVENTO,NOMBRE_DEPORTE)
-VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}') """.format(request.json ['id de la actividad'],
-request.json['nombre de la actividad'],
-
-request.json['texto'],
-
-request.json['km'],
-
-request.json['frecuencia cardiaca'],
-
-request.json['kcal'],
-            
-request.json['duracion'],
-
-request.json['id del evento'],
-
-request.json['nombre del deporte']
-    
+        (ID_ACTIVIDAD,NOMBRE_ACTIVIDAD, TEXTO, KM, FC,KCAL, DURACION,ID_EVENTO,NOMBRE_DEPORTE)
+        VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}') """.format(request.json ['ID_ACTIVIDAD'],
+        request.json['NOMBRE_ACTIVIDAD'],
+        request.json['TEXTO'],
+        request.json['KM'],
+        request.json['FC'],
+        request.json['KCAL'],
+        request.json['DURACION'],
+        request.json['ID_EVENTO'],
+        request.json['NOMBRE_DEPORTE']
             )
         
         cursor. execute(sql)
@@ -195,34 +151,33 @@ request.json['nombre del deporte']
 #confirma acción inserción
     #print(request.json)
         return jsonify({'mensaje': 'actividad registrada'})
-    except Exception as ex:
-        return jsonify({'mensaje': "error"})
-    
+    except Exception as ex: 
+        return jsonify({'mensaje': ex}) 
 
-#eventousuario
-@app.route('/cursos', methods= ['POST'])
-def registrar_eventousuario():
+@app.route('/usuarioactividad', methods= ['POST'])
+def registrar_usuarioactividad():
     #print(request)
     try:
         cursor = conexion.connection.cursor()
-        sql = """INSERT INTO eventousuario
-(ID_EVENTO,ID_USUARIO)
-VALUES ('{0}','{1}','{2}') """.format(request.json ['id del evento'],
-request.json['id del usuario']
-            
+        sql = """INSERT INTO usuarioactividad
+        (ID_USUARIO,ID_ACTIVIDAD)
+        VALUES ('{0}','{1}') """.format(request.json ['ID_USUARIO'],
+        request.json['ID_ACTIVIDAD']
             )
-        
+     
         cursor. execute(sql)
         conexion.connection.commit()
 #confirma acción inserción
     #print(request.json)
-        return jsonify({'mensaje': 'eventousuario registrado'})
-    except Exception as ex:
-        return jsonify({'mensaje': "error"})
+        return jsonify({'mensaje': 'actividad registrada'})
+    except Exception as ex: 
+        return jsonify({'mensaje': ex})    
 
 
 
-##leer
+
+
+##lecturas
 @app.route('/eventos/<codigo>', methods= ['GET'])
 def leer_evento(codigo):
     try:
@@ -238,6 +193,7 @@ def leer_evento(codigo):
             return jsonify({'mensaje': 'ciudad no encontrada'})
     except Exception as ex:
         return jsonify({'mensaje': 'error'})
+    
 
 @app.route('/eventos/lugar/<codigo>', methods= ['GET'])
 def leer_eventociudad(codigo):
@@ -245,27 +201,55 @@ def leer_eventociudad(codigo):
         cursor =conexion.connection.cursor()
         sql = "SELECT * FROM eventos WHERE LOCALIZACION LIKE '%{0}'".format(codigo)
         cursor.execute(sql)
-        fila=cursor.fetchone()
-        if fila != None:
-            evento={'ID_EVENTO': fila[0],'FECHA': str(fila[1]),'HORA':str(fila[2]),
-                      'PRECIO':fila[3],'LOCALIZACION': fila[4]}
-            return jsonify({'ciudades': evento,'mensaje': 'ciudad encontrada'})
-        else:
+        datos=cursor.fetchall()
+        eventos =[]
+        if datos == None:
             return jsonify({'mensaje': 'ciudad no encontrada'})
+        else: 
+            eventos =[]
+            for fila in datos:
+                evento={'ID_EVENTO': fila[0],'FECHA': str(fila[1]),'HORA':str(fila[2]),
+                        'PRECIO':fila[3],'LOCALIZACION': fila[4]}
+                eventos.append(evento)
+            print(eventos)
+            return jsonify({'ciudades': eventos,'mensaje': 'ciudad encontrada'})
     except Exception as ex:
         return jsonify({'mensaje': 'error'})
+    
+    
 
- 
+@app.route('/ultimousuario', methods= ['GET'])
+def ultimo_usuario():
+    try:
+        cursor =conexion.connection.cursor()
+        sql = "SELECT ID_USUARIO FROM USUARIOS ORDER BY CAST(SUBSTR(ID_USUARIO,12,LENGTH(ID_USUARIO)-11) AS UNSIGNED)"
+        cursor.execute(sql)
+        datos= cursor.fetchall()
+        fila =datos[-1]
+        actividad={'ID_ACTIVIDAD': fila[0]}
+        print(datos)
+        return jsonify({'usuario': actividad ,'mensaje': 'actividad'})
 
-def pagina_no_encontrada(error):
-    return "<h1> la pagina no existe </h1>", 404
-
-if __name__=="__main__":
-    app.config.from_object(config['development'])
-    app.register_error_handler(404,pagina_no_encontrada)
-    app.run()
+    except Exception as ex:
+        return jsonify({'mensaje': ex})
+         
 
 
+@app.route('/ultimaactividad', methods= ['GET'])
+def ultima_actividad():
+    try:
+        cursor =conexion.connection.cursor()
+        sql = "SELECT ID_ACTIVIDAD FROM ACTIVIDADES ORDER BY CAST(SUBSTR(ID_ACTIVIDAD,7,LENGTH(ID_ACTIVIDAD)-6) AS UNSIGNED)"
+        cursor.execute(sql)
+        datos= cursor.fetchall()
+        actividades =[]
+        fila =datos[-1]
+        actividad={'ID_ACTIVIDAD': fila[0]}
+        print(datos)
+        return jsonify({'actividad': actividad ,'mensaje': 'actividad'})
+
+    except Exception as ex:
+        return jsonify({'mensaje': ex})
 
 @app.route('/usuario_deportes/<usuariocotilleo>', methods=['GET'])
 def listar_usuario_deportes(usuariocotilleo):
@@ -274,9 +258,10 @@ def listar_usuario_deportes(usuariocotilleo):
         sql= """
         SELECT A.NOMBRE_DEPORTE, COUNT(*)
         FROM ACTIVIDADES A, USUARIOACTIVIDAD UA
-        WHERE A.ID_ACTIVIDAD=UA.ID_ACTIVIDAD AND UA.ID_USUARIO= '{0}'".format(usuariocotilleo)
-        GROUP BY A.NOMBRE_DEPORTE;
-        """
+        WHERE A.ID_ACTIVIDAD=UA.ID_ACTIVIDAD AND UA.ID_USUARIO= '{0}'
+        GROUP BY A.NOMBRE_DEPORTE""".format(usuariocotilleo)
+        
+        
         # Ejecutar la consulta con el valor de id_usuario
         cursor.execute(sql)
         datos = cursor.fetchall()
@@ -291,43 +276,13 @@ def listar_usuario_deportes(usuariocotilleo):
             deportes_por_usuario[deporte] = cantidad
 
         # Retornar el diccionario en formato JSON
-       return jsonify({
-    'deportes_por_usuario': f'Veces que {usuariocotilleo} ha hecho estos deportes',
-    'mensaje': 'Actividades listadas correctamente'})
-
-    except Exception as e:
-        return jsonify({'mensaje': 'Error al listar actividades', 'error': str(e)})
-
-
-
-    @app.route('/usuarios_actividades', methods=['GET'])
-def listar_usuarios_actividades():
-    try:
-        cursor = conexion.connection.cursor()
-        sql= """
-        SELECT U..NOMBRE_USUARIO, COUNT(UA.ID_ACTIVIDAD)
-        FROM USUARIOACTIVIDAD UA, USUARIOS U
-        WHERE UA.ID_USUARIO=U.ID_USUARIO
-        GROUP BY UA.ID_USUARIO
-        """
-        # Ejecutar la consulta con el valor de id_usuario
-        cursor.execute(sql)
-        datos = cursor.fetchall()
-
-        # Diccionario para guardar las actividades por usuario
-        usuarios_actividades = {}
-
-        # Construir el diccionario con los datos obtenidos
-        for fila in datos:
-            usuario = fila[0]  # Nombre del deporte
-            cantidad = fila[1]  # Número de veces que hizo el deporte
-            usuarios_actividades[usuario] = cantidad
-
-        # Retornar el diccionario en formato JSON
-        return jsonify({'Actividades de cada usuario': usuarios_actividades, 'mensaje': 'Actividades de cada usuario'})
+        return jsonify({'deportes_por_usuario': deportes_por_usuario, 'mensaje': 'Actividades listadas correctamente'})
     
     except Exception as e:
         return jsonify({'mensaje': 'Error al listar actividades', 'error': str(e)})
+
+
+
 
 @app.route('/usuarios/contrasena/<codigo>', methods= ['GET'])
 def obtener_contrasena(codigo):
@@ -362,3 +317,10 @@ def obtener_nombre(codigo):
         return jsonify({'mensaje': 'Error al obtener el usuario', 'error': str(ex)})
 
 
+def pagina_no_encontrada(error):
+    return "<h1> la pagina no existe </h1>", 404
+
+if __name__=="__main__":
+    app.config.from_object(config['development'])
+    app.register_error_handler(404,pagina_no_encontrada)
+    app.run()
